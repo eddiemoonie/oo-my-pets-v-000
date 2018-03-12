b0893a01aad7b256669e155caa37468910bda844
Eddie Moon <eddiemoonie@gmail.com>
2018-03-12 17:18:48 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
-  # code goes here
+
+    attr_accessor :name, :pets
+    attr_reader :species
+    OWNERS = []
+
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+    OWNERS << self
+  end
+
+  def say_species
+    "I am a #{species}"
+  end
+
+  def self.all
+    OWNERS
+  end
+
+  def self.reset
+    OWNERS.clear
+  end
+
+  def self.count
+    OWNERS.count
+  end
+
+  def walk_dogs
+
+  end
+
 end
